3d7a2003889c3eceb5a253058afefd8a3b6ffedc
ABBOS <turgunov1296914@gmail.com>
2024-01-15 16:19:42 +0500

    Commit-3

--- src/uz/pdp/firstHomeworkWithGit/Main.java
+++ src/uz/pdp/firstHomeworkWithGit/Main.java
@@ -3,6 +3,6 @@ package uz.pdp.firstHomeworkWithGit;
 public class Main {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
-        System.out.println("Hello world");
+
     }
 }
